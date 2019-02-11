ab5080e56ab461d41a48de6ec0285628bfe07865
Pattie Reaves <pattie@alley.co>
2019-02-10 20:18:37 -0700

    Start plugin.

---
+++ plugins/hello-alexa/hello-alexa.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name: Hello Alexa
+ * Description: A hello-world example of how to use VoiceWP and Alexa.
+ * Plugin URI: https://github.com/pattiereaves/hello-alexa/tree/master/plugins/hello-alexa
+ * Author: pattiereaves
+ * Author URI: http://twitter.com/pazzypunk
+ * Version: 1.0.0
+ * Text Domain: hello-alexa
+ * License: MIT
+ */
